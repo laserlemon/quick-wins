1cb8d4b08c1ae9df2617e6e553210066495e4096
Steve Richert <steve.richert@gmail.com>
2014-04-30 17:23:31 -0400

    Add a performance spec to run and measure a taxing scenario

---
+++ spec/performance/message_list_spec.rb
@@ -0,0 +1,37 @@
+require "spec_helper"
+
+describe "Message List Performance", type: :feature do
+  let!(:user) { create(:user) }
+
+  before do
+    create_list(:message, 1_000, recipient: user)
+
+    log_in(user)
+  end
+
+  def measure(&block)
+    time = Benchmark.realtime(&block)
+    example.description << " (#{time})"
+  end
+
+  it "is fast!" do
+    measure do
+      # Visit the page
+      visit(messages_path)
+
+      10.times do |i|
+        # Refresh the page twice
+        2.times { visit(current_path) }
+
+        # See the messages
+        dom_messages = DOM::Message.all
+
+        # Select the first 10 unread messages
+        dom_messages.slice(i * 100, 100).each(&:select)
+
+        # Mark the selected messages as read
+        click_button("Mark as Read")
+      end # Rinse, repeat
+    end
+  end
+end
