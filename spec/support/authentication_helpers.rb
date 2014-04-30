c4705c70dd65216462c5c56210237de43b96cef7
Steve Richert <steve.richert@gmail.com>
2014-04-30 15:23:31 -0400

    Add the log_in test helper for authenticating feature specs

---
+++ spec/support/authentication_helpers.rb
@@ -0,0 +1,27 @@
+module AuthenticationHelpers
+  def log_in(user)
+    page.cookies[:current_user_id] = user.id
+  end
+end
+
+module Capybara
+  class Session
+    def cookies
+      @cookies ||= ActionDispatch::Cookies::CookieJar.new(key_generator)
+    end
+
+    private
+
+    def key_generator
+      @key_generator ||= ActiveSupport::LegacyKeyGenerator.new(SecureRandom.hex)
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include(AuthenticationHelpers)
+
+  config.before(type: :feature) do
+    ActionDispatch::Request.any_instance.stub(:cookie_jar) { page.cookies }
+  end
+end
