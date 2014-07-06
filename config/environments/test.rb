7c2185d05bc714a46c1c85fb69db4cad62665188
Steve Richert <steve.richert@gmail.com>
2014-07-06 10:36:06 -0400

    Disable query caching to accent query-related optimizations

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Disable query caching to more clearly demonstrate query-related
+  # performance wins.
+  config.middleware.delete "ActiveRecord::QueryCache"
 end
